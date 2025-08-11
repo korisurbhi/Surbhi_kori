2f63b283330b377146bf083f298f378f7f32be62
korisurbhi <surbhikori11096@gmail.com>
2025-08-11 10:50:40 +0530

    Create function.py

---
+++ python/function/function.py
@@ -0,0 +1,44 @@
+#1. Function Without parameters
+
+#function declaration
+def greet():
+    print("Hello! Welcome ot Python.")
+#call the function
+greet()
+
+
+#2. Funtion With parameters
+
+#function declaration
+def greet(name):
+    print("Hello", name)
+# call the function
+greet("surbhi")
+
+
+#3. Function with return value
+
+#function declaration
+def add(a, b):
+    return a + b
+#Call the function and print result
+result = add (5, 3)
+print("Addtion is : ", result)
+
+
+#4. Function to check Even or Odd
+
+#function declaration
+def check_even_odd(number):
+    if number % 2 ==0:
+        print(f"The number {number} is Even.")
+    else:
+        print(f"The number {number} is Odd.")
+#call the function
+check_even_odd(7)
+check_even_odd(164)
+
+
+#5. 
+
+#.
