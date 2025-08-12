547fe481d1ca03ab96cad25c287abac24aca0462
korisurbhi <surbhikori11096@gmail.com>
2025-08-12 10:27:18 +0530

    function

--- python/function/function.py
+++ python/function/function.py
@@ -39,6 +39,46 @@ check_even_odd(7)
 check_even_odd(164)
 
 
-#5. 
+#5. Function to find Factorial 
 
-#.
+#function declaration 
+def factorial(n):
+    result = 1
+    for i in range (1, n+1):
+        result *= i
+    return result
+#call the function
+print ("Factorial of 5 is : ", factorial(5))
+
+
+#6. Function to find Maximum of two numbers
+
+#function declaration
+def maximum(x, y):
+    if x > y:
+        return x
+    else:
+        return y 
+#call the function
+print("Maximum is :", maximum(10, 20))
+
+
+#7. Function to print table of a Number
+
+#function declaration
+def print_table(n):
+    for i in range (1, 11):
+        print(f"{n} * {i} = {n*i}")
+#call the function
+print_table(5)
+
+
+#8. Function with default argument 
+
+#function declaration
+def greet (name="student"):
+    print ("hello", name)
+
+#call the function
+greet()         #uses default
+greet("surbhi") #uses given value
