4ab1cfe558a3cac1a65abda12f88868a15bb69cd
korisurbhi <surbhikori11096@gmail.com>
2026-08-10 12:14:57 +0530

    adding individual hra and da

--- sem4/OOPS PRACTICAL/2nd practical- structure with multiple employee.cpp
+++ sem4/OOPS PRACTICAL/2nd practical- structure with multiple employee.cpp
@@ -1,46 +1,53 @@
 #include<iostream>
 #include<string>
 using namespace std;
-struct Employee
+
+struct employee
 {
 	int empID;
 	string name;
-	float basic,hra,da,gross;
+	float basic, hra, da, gross;
 };
 
 int main()
 {
-	int i,n;
+	int n,i,h,d;
+	
 	cout<<"Enter Employee Numbers:";
 	cin>>n;
-	Employee emp[n];
+	
+	employee emp[n];
 	
 	for(i=0;i<n;i++)
 	{
-		cout<<"Enter Employee ID:";
+		cout<<"Enter Employee id:";
 		cin>>emp[i].empID;
 		
 		cout<<"Enter Employee Name:";
 		cin>>emp[i].name;
-	
+		
 		cout<<"Enter Basic Salary:";
 		cin>>emp[i].basic;
-	
-		emp[i].hra= 0.25*emp[i].basic;
-		emp[i].da=0.30*emp[i].basic;
-		emp[i].gross=emp[i].hra+emp[i].da+emp[i].basic;
+		
+		cout<<"Enter hra:";
+		cin>>h;
+		
+		cout<<"Enter da:";
+		cin>>d;
+		
+		emp[i].hra=(h/100)*emp[i].basic;
+		emp[i].da=(d/100)*emp[i].basic;
+		emp[i].gross=emp[i].da+emp[i].hra+emp[i].basic;
 	}
-
-	cout<<"-----Employee Details-----"<<endl;
 	
 	for(i=0;i<n;i++)
 	{
-		
-		cout<<"Employee ID:"<<emp[i].empID<<endl;
+		cout<<"Employee id:"<<emp[i].empID<<endl;
 		cout<<"Employee Name:"<<emp[i].name<<endl;
+		cout<<"Basic Salary:"<<emp[i].basic<<endl;
 		cout<<"HRA:"<<emp[i].hra<<endl;
 		cout<<"DA:"<<emp[i].da<<endl;
-		cout<<"Gross salary:"<<emp[i].gross<<endl;
+		cout<<"Gross Salary:"<<emp[i].gross<<endl;
 	}
 	
 	return 0;
